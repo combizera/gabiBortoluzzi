706a605ba7a232e33860421b77b4af5b7f809ea8
Ygor Combi <ycombi349@gmail.com>
2022-08-31 09:14:20 -0300

    read-more

--- js/more.js
+++ js/more.js
@@ -1,27 +1,41 @@
 // Script Veja Mais
-const txt = document.querySelector(".txt-more");
-const txtInitialHeight = txt.clientHeight;
-const overflow = document.querySelector(".overflow");
-const btnReadMore = document.querySelector(".toggleMore");
+$(document).ready(function () {
+  var showChar = 250;
+  var ellipsestext = "...";
+  var moretext = "Mostrar mais >";
+  var lesstext = "Mostrar menos";
 
-btnReadMore.addEventListener("click", initToggle);
+  $(".more").each(function () {
+    var content = $(this).html();
 
-function initToggle(e) {
-  txt.style.maxHeight =
-    e.target.dataset.state === "more"
-      ? `${txt.scrollHeight}px`
-      : `${txtInitialHeight}px`;
+    if (content.length > showChar) {
+      var c = content.substr(0, showChar);
+      var h = content.substr(showChar, content.length - showChar);
 
-  e.target.setAttribute(
-    "data-state",
-    e.target.dataset.state === "more" ? "less" : "more"
-  );
+      var html =
+        c +
+        '<span class="moreellipses">' +
+        ellipsestext +
+        '&nbsp;</span><span class="morecontent"><span>' +
+        h +
+        '</span>&nbsp;&nbsp;<a href="" class="morelink">' +
+        moretext +
+        "</a></span>";
 
-  e.taget.innerHTML =
-    e.target.datast.state === "more" ? "Show more" : "Show Less";
+      $(this).html(html);
+    }
+  });
 
-  overflow.setAttribute(
-    "data-state",
-    e.target.dataset.state === "more" ? "visible" : "hidden"
-  );
-}
+  $(".morelink").click(function () {
+    if ($(this).hasClass("less")) {
+      $(this).removeClass("less");
+      $(this).html(moretext);
+    } else {
+      $(this).addClass("less");
+      $(this).html(lesstext);
+    }
+    $(this).parent().prev().toggle();
+    $(this).prev().toggle();
+    return false;
+  });
+});
